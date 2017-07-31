f7457c28382cc9bb041f6d359525f364c7c22ff5
Garrett Cullen <Garth619@users.noreply.github.com>
2017-07-31 15:45:52 -0700

    push

---
+++ wp-content/themes/kaufman/homepage_template_parts/section-2.php
@@ -0,0 +1,15 @@
+<section class="section_two">
+	
+	<div class="intro_form_wrapper">
+		
+		
+		
+	</div><!-- intro_form_wrapper -->
+	
+	<div class="case_results">
+		
+		
+		
+	</div><!-- case_results -->
+	
+</section><!-- section_two -->
