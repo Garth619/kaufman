ddb9047679526f9e5c6f37916cf73db71779f78a
Garrett Cullen <Garth619@users.noreply.github.com>
2017-07-31 16:18:57 -0700

    push

--- wp-content/themes/kaufman/homepage_template_parts/section-2.php
+++ wp-content/themes/kaufman/homepage_template_parts/section-2.php
@@ -2,9 +2,22 @@
 	
 	<div class="intro_form_wrapper">
 		
+		<span class="form_parts">1 of 2</span><!-- form_parts -->
 		
+		<svg id="red_line_form" viewBox="0 0 52.79 8.27">
+		
+			<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+		</svg><!-- red_line_form -->
+		
+		<div class="form_container">
+			
+			<span class="injury_title">What type of injury have you suffered?</span><!-- injury_title -->
+			
+		</div><!-- form_container -->
 		
 	</div><!-- intro_form_wrapper -->
+	
 	
 	<div class="case_results">
 		
