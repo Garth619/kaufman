cf6faa354a7edab0bbf6920ac1f10272b0e4a212
Garrett Cullen <Garth619@users.noreply.github.com>
2017-07-31 12:54:21 -0700

    push

--- wp-content/themes/kaufman/page-home.php
+++ wp-content/themes/kaufman/page-home.php
@@ -8,6 +8,42 @@
 
 <section class="intro_banner">
 	
+	<div class="intro_banner_content">
+		
+		
+		
+		<span class="intro_header">
+		
+			<div class="play_wrapper">
+				
+				<span class="learn">Learn about our law<br/> firm in 90 seconds</span><!-- learn -->
+				
+				<svg id="red_line_intro" viewBox="0 0 136.39 11.05">
+				
+					<defs>
+					
+						
+					
+					</defs>
+					
+					<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+				</svg>
+				
+		
+				<a class="play_button" href="">
+					<img src="<?php bloginfo('template_directory');?>/images/play-button-desktop.png"/>
+				</a> 
+		
+			</div><!-- play_wrapper -->
+		
+		
+		Representing<br/> the Injured for more than <strong>40</strong> Years
+		
+		</span><!-- intro_header -->
+		
+	</div><!-- intro_banner_content -->
+	
 	<video src="<?php bloginfo('template_directory');?>/images/videobg.mp4" loop autoplay></video>
 	
 </section><!-- intro_banner -->
