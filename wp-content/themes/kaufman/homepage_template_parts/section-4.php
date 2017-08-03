04b4221ee643f614ba7cf1ff0effca4ec182855e
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-03 16:21:49 -0700

    push

--- wp-content/themes/kaufman/homepage_template_parts/section-4.php
+++ wp-content/themes/kaufman/homepage_template_parts/section-4.php
@@ -108,6 +108,91 @@
 			
 		</div><!-- why_choose_bg_wrapper -->
 		
+		<div style="clear:both"></div>
+		
 	</div><!-- why_choose_wrapper -->
+	
+	
+	
+	<div class="section_four_lower">
+		
+		<div class="section_four_lower_wrapper">
+			
+			<span class="large_header">Helping Clients Become Whole Again</span><!-- large_header -->
+			
+			<svg id="section_four_gold_threelines" data-name="section three gold twolines" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 190.06 86"><defs><style>.cls-1{fill:none;}</style></defs><title>new</title>
+			
+			<polyline points="145.2 9 133 0 121.9 8.2 110.8 0 99.7 8.2 88.7 0 77.6 8.2 66.5 0 55.4 8.2 44.3 0 33.3 8.2 22.2 0 10 9"/>
+			<polyline points="190.06 18.9 177.86 9.9 166.76 18.1 155.66 9.9 144.56 18.1 133.56 9.9 122.46 18.1 111.36 9.9 100.26 18.1 89.16 9.9 78.16 18.1 67.06 9.9 54.86 18.9"/>
+			<polyline points="168.2 29 156 20 144.9 28.2 133.8 20 122.7 28.2 111.7 20 100.6 28.2 89.5 20 78.4 28.2 67.3 20 56.3 28.2 45.2 20 33 29"/>
+		
+			</svg>
+			
+			<span class="sub_header">read our most recent client testimonials</span><!-- sub_header -->
+			
+			<div class="testi_wrapper">
+				
+				<div class="testi_col">
+					
+					<span class="testi_excerpt">"I don't know what I would had done without your help."</span><!-- testi_excerpt -->
+					
+					<svg id="testi_red_line_one" class="testi_red_line" viewBox="0 0 52.79 8.27">
+		
+						<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+					</svg><!-- red_line_form -->
+					
+					<div class="testi_content">
+						
+						<p>I have never been in this kind of situation. I had no idea what was I entering. Kaufman Law Firm made sure I understood what and explained to me an my family every step of the way. Not one moment did I feel unprotected. I knew Kaufman Law had my back. I don't know what I would had done without your help. My family and I are thankful and highly appreciate all the work you have done for me. Once more. Thank you very much to you and the rest of the team.</p>
+						
+					</div><!-- testi_content -->
+					
+				</div><!-- test_col -->
+				
+				<div class="testi_col">
+					
+					<span class="testi_excerpt">"This is a very professional practice and everyone there is caring and willing to help you in anyway they can."</span><!-- testi_excerpt -->
+					
+					<svg id="testi_red_line_two" class="testi_red_line" viewBox="0 0 52.79 8.27">
+		
+						<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+					</svg><!-- red_line_form -->
+					
+					<div class="testi_content">
+						
+						<p>After calling several law firms and even hiring one, I still dint feel comfortable until I made the call to Kaufman Law. I fired the attorney I was working with and hired Mr. Marvin and his wonderful assistant Amy. Thank you both so much. This is a very professional practice and everyone there is caring and willing to help you in anyway they can.</p>
+						
+					</div><!-- testi_content -->
+					
+				</div><!-- test_col -->
+				
+				<div class="testi_col">
+					
+					<span class="testi_excerpt">"They took care of every little thing and dealt with it really positively."</span><!-- testi_excerpt -->
+					
+					<svg id="testi_red_line_three" class="testi_red_line" viewBox="0 0 52.79 8.27">
+		
+						<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+					</svg><!-- red_line_form -->
+					
+					<div class="testi_content">
+						
+						<p>Kaufman Law is a law firm which you would want to be representing you and handling your case. It was a pleasure working with both Amy Hurst and Kenneth P. Raley. They made sure I was aware of everything that was happening with the case and very humble people. They took care of every little thing and dealt with it really positively. I was stressed free knowing that my case was in the best hands possible.</p>
+						
+					</div><!-- testi_content -->
+					
+				</div><!-- test_col -->
+				
+			</div><!-- testi_wrapper -->
+			
+		</div><!-- section_four_lower_wrapper -->
+		
+	</div><!-- section_lower_content -->
+	
+	
+	
 	
 </section><!-- section_four -->
