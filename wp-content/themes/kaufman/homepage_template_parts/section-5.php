b93ea3349d94774f2d5933ebbed224aa6424999f
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-04 15:05:37 -0700

    push

---
+++ wp-content/themes/kaufman/homepage_template_parts/section-5.php
@@ -0,0 +1,50 @@
+<section class="section_five">
+	
+	<div class="section_five_inner_wrapper">
+		
+		<div class="featured_attorney">
+			
+			<div class="featured_att_image">
+				
+				<img class="" src="<?php bloginfo('template_directory');?>/images/attorneys/kaufman.jpg"/><!-- featured_att_image -->
+				
+			</div><!-- featured_att_image-->
+			
+			
+			
+			<div class="featured_attorney_content">
+				
+				
+				<div class="founding_wrapper">
+				
+					<span class="founding_attorney">Founding Attorney</span><!-- founding_attorney -->
+					
+					<svg id="founding_red_line" viewBox="0 0 52.79 8.27">
+		
+						<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+					</svg><!-- red_line_form -->
+					
+				</div><!-- founding_wrapper -->
+				
+				<span class="name">Jeffery Kaufman</span><!-- name -->
+				
+				<span class="att_content">Practicing injury law since 1977, Jeffrey has recovered millions of dollars for clients. He has secured one of the largest verdicts in Georgia’s history— a $25 million dollar verdict for a medical malpractice case.</span><!-- att_content -->
+				
+				<a class="view_button" href="">View Attorney Profile <img src="<?php bloginfo('template_directory');?>/images/white-arrow-r.png"/></a><!-- view_button -->
+				
+			</div><!-- featured_attorney_content -->
+			
+			
+			
+		</div><!-- featured_attorney -->
+		
+		
+		<div class="att_slideshow"></div><!-- att_slideshow -->
+		
+		
+		
+	</div><!-- section_five_inner_wrapper -->
+	
+	
+</section><!-- section_five -->
