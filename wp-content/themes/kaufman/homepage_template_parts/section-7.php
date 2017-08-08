29b8661da5e506ad44f417c3c6fa48aee290056d
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-08 16:30:17 -0700

    push

---
+++ wp-content/themes/kaufman/homepage_template_parts/section-7.php
@@ -0,0 +1,59 @@
+<section class="section_seven">
+	
+	<div class="news_header_wrapper">
+		
+		<svg id="news_white_twolines" viewBox="0 0 51.9 10.39"><defs><style>.cls-1{fill:none;}</style></defs><title>new</title>
+			
+			<polyline transform="translate(0,3)" class="cls-1" points="0 10.39 8.7 4 17.3 10.39 26 4 34.7 10.39 43.3 4 51.9 10.39"/>
+			<polyline class="cls-1" points="0 6.39 8.7 0 17.3 6.39 26 0 34.7 6.39 43.3 0 51.9 6.39"/>
+			
+		</svg><!-- news_header_wrapper -->
+		
+		<span class="large_header">Stay Informed On<span class="lower_header"> Injury News</span></span><!-- large_header -->
+		
+	</div><!-- news_header_wrapper -->
+	
+	<div class="news_slidehow_wrapper">
+		
+		<div class="news_slidehow">
+			
+			
+			<a class="news_single_link" href="">
+				
+				
+				<div class="single_slide_wrapper">
+					
+					<div class="inner_slide_content">
+					
+					<svg id="news_post_twolines" viewBox="0 0 51.9 10.39"><defs><style>.cls-1{fill:none;}</style></defs><title>new</title>
+			
+						<polyline transform="translate(0,3)" class="cls-1" points="0 10.39 8.7 4 17.3 10.39 26 4 34.7 10.39 43.3 4 51.9 10.39"/>
+						<polyline class="cls-1" points="0 6.39 8.7 0 17.3 6.39 26 0 34.7 6.39 43.3 0 51.9 6.39"/>
+			
+					</svg><!-- news_header_wrapper -->
+					
+					<span class="news_date">April 17, 2017</span><!-- news_date -->
+					
+					<span class="post_title">Tips for Preventing Motorcycle Accidents</span><!-- post_title -->
+					
+					<span class="excerpt">Motorcycles are inherently less safe than cars, lacking many of the safety features that help minimize the vulnerability of drivers and passengers during an accident. While they may be dangerous, the ...</span><!-- excerpt -->
+					
+					</div><!-- inner_slide_content -->
+					
+					<div class="read_more_wrapper"> 
+						
+						<span class="read_more">Read More</span>
+						
+						<img src="<?php bloginfo('template_directory');?>/images/white-arrow-r.png"/>
+						
+					</div><!-- read_more -->
+					
+				</div><!-- single_slide_wrapper -->
+				
+			</a><!-- news_single_link -->
+			
+		</div><!-- news_slidehow -->
+		
+	</div><!-- news_slidehow_wrapper -->
+	
+</section><!-- section_seven -->
