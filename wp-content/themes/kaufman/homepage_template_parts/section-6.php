1e60bb6df5ff7e974749e16e46bc8690696819b8
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-08 09:46:28 -0700

    push

--- wp-content/themes/kaufman/homepage_template_parts/section-6.php
+++ wp-content/themes/kaufman/homepage_template_parts/section-6.php
@@ -15,22 +15,187 @@
 			<span class="large_header"><strong>Areas</strong><br/> of Practice</span>
 			
 			<ul>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
-				<li><a href="">Auto Accidents</a></li>
+				
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				<li>
+				
+					<a href="">
+					
+						<span>Auto Accidents</span>
+					
+						<svg id="list_item_white_line" viewBox="0 0 52.79 8.27">
+		
+							<polyline class="red_line_two" points="0.44 7.33 9.14 0.94 17.74 7.33 26.44 0.94 35.14 7.33 43.74 0.94 52.34 7.33"/>
+			
+							</svg><!-- red_line_form -->
+					
+					
+					</a>
+				</li>
+				
+				
+				
+				
+				
+				
 			</ul>
 			
 		</div><!-- pa_sidebar -->
 		
 		<div class="pa_content content">
 			
-			
+		<p>
+				
+				
+				<span class="first_letter"><span>A</span></span>t Kaufman Law, P.C., we understand that our clients are not always able to come to our office after they have been the victim of a serious injury. If you are in this predicament, we are willing to come see you in the hospital or at your home in order to get a jumpstart on your case. When we work with you, our Atlanta catastrophic injury attorneys will utilize our experience, resources, and passion for justice to help you secure the most favorable outcome possible. We will not hesitate to go to battle for you in the courtroom, and we will ensure your rights are protected against any insurance company, business or corporation. Rest-assured that in working with our firm, we will ensure your legal rights are protected so you can focus on moving forward.</p>
+
+			<blockquote>MAKING A DIFFERENCE IN EVERY CLIENT'S LIFE</blockquote>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo totam rema.</p>
 			
 			
 		</div><!-- pa_content -->
