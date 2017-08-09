9e4b0f139d48a3cf9538b46d3f348e4351ae3978
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-09 16:10:50 -0700

    push

--- wp-content/themes/kaufman/footer.php
+++ wp-content/themes/kaufman/footer.php
@@ -1,10 +1,34 @@
 <footer>
 	
+	<div class="inner_footer">
+		
+		<div class="footer_left">
+			
+			<svg id="footer_gold_twolines" viewBox="0 0 158.39 21.46">
+				
+				<polyline class="red_line_one" points="135.79 20.66 123.59 11.66 112.49 19.86 101.39 11.66 90.29 19.86 79.29 11.66 68.19 19.86 57.09 11.66 45.99 19.86 34.89 11.66 23.89 19.86 12.79 11.66 0.59 20.66"/>				<polyline class="red_line_two" points="157.79 10.24 145.59 1.25 134.49 9.45 123.39 1.25 112.29 9.45 101.29 1.25 90.19 9.45 79.09 1.25 67.99 9.45 56.89 1.25 45.89 9.45 34.79 1.25 22.59 10.24"/>
+				
+			</svg><!-- intro_red_twolines -->
+			
+			<span class="firm_title"><strong>Kaufman Law, P.C.</strong><br/> Atlanta Personal Injury Attorney</span><!-- firm_title -->
+			
+			<a class="address" href="" target="_blank">100 Galleria Pkwy SE, Suite 1100,<br/> Atlanta, GA 30339</a>
+			
+			<a class="get_directions" href="">Get Directions</a>
+			
+			<span class="office_hours_title">Office Hours</span><!-- office_hours_title -->
+			
+			<span class="hours">Mon&nbsp;-&nbsp;Fri&nbsp;<span class="dot">&middot;</span>&nbsp;8am&nbsp;-&nbsp;5pm</span><!-- hours -->
+			
+			
+		</div><!-- footer_left -->
+		
+		<div class="footer_right"></div><!-- footer_right -->
+		
+	</div><!-- inner_footer -->
 	
 	
 </footer>
-
-
 
 </section><!-- start_here_track -->
 
