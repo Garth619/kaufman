d6e22272c365b56b1b41d2491d513ff3cebbae5f
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-24 11:23:11 -0700

    push

--- wp-content/themes/kaufman/homepage_template_parts/section-7.php
+++ wp-content/themes/kaufman/homepage_template_parts/section-7.php
@@ -2,14 +2,18 @@
 	
 	<div class="news_header_wrapper">
 		
-		<svg id="news_white_twolines" viewBox="0 0 51.9 10.39"><defs><style>.cls-1{fill:none;}</style></defs><title>new</title>
-			
-			<polyline transform="translate(0,3)" class="cls-1" points="0 10.39 8.7 4 17.3 10.39 26 4 34.7 10.39 43.3 4 51.9 10.39"/>
-			<polyline class="cls-1" points="0 6.39 8.7 0 17.3 6.39 26 0 34.7 6.39 43.3 0 51.9 6.39"/>
-			
-		</svg><!-- news_header_wrapper -->
+		<div class="inner_news">
+		
+			<svg id="news_white_twolines" viewBox="0 0 51.9 10.39"><defs><style>.cls-1{fill:none;}</style></defs><title>new</title>
+				
+				<polyline transform="translate(0,3)" class="cls-1" points="0 10.39 8.7 4 17.3 10.39 26 4 34.7 10.39 43.3 4 51.9 10.39"/>
+				<polyline class="cls-1" points="0 6.39 8.7 0 17.3 6.39 26 0 34.7 6.39 43.3 0 51.9 6.39"/>
+				
+			</svg><!-- news_header_wrapper -->
 		
 		<span class="large_header">Stay Informed On<span class="lower_header"> Injury News</span></span><!-- large_header -->
+		
+		</div><!-- inner_news -->
 		
 	</div><!-- news_header_wrapper -->
 	
