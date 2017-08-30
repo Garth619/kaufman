a2b3567997125422bfe479e2e6f4da38793ccd26
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-30 14:45:11 -0700

    push

--- wp-content/themes/kaufman/page-contact.php
+++ wp-content/themes/kaufman/page-contact.php
@@ -18,9 +18,73 @@
 		<div class="internal_content content">
 			
 			
-			Coming Shortly
 			
-			<?php //get_template_part( 'loop', 'index' );?>
+			<div class="contact_wrapper">
+				
+				<span class="contact_excerpt">At Kaufman Law, P.C., we have been protecting the rights of injured individuals for over 40 years. Contact us today with your&nbsp;case!</span><!-- contact_excerpt -->
+				
+				<svg id="contact_gold_line" viewBox="0 0 136.39 11.05">
+				
+					<defs>
+					
+						
+					
+					</defs>
+					
+					<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+				</svg><!-- red_line_intro -->
+				
+				<div class="contact_info">
+					
+					
+					<div class="contact_info_left">
+					
+						<span class="call_title">Call</span><!-- call_title -->
+					
+						<a href="">(678) 712-1115</a>
+					
+						<span class="gold_line"></span><!-- gold_line -->
+					
+						<p>Our Atlanta personal injury lawyers have achieved million-dollar results for our clients. Whether we are representing a slip and fall victim or someone negligently injured in a car accident, 
+we do everything possible to secure maximum compensation. Our clients are so important to us that we make ourselves available around the clock and offer bilingual services.</p>
+
+						<p>If you need help, please fill out the 
+form to request a complimentary initial consultation regarding your case.</p>
+
+					</div><!-- contact_info_left -->
+					
+					<div class="contact_info_right">
+						
+						<div class="location_wrapper">
+							
+							<span class="contact_location_title">Location</span><!-- contact_location_title -->
+							
+							<a class="internal_address" href="https://www.google.com/maps?q=100+Galleria+Pkwy+SE,+Suite+1100&oe=utf-8&um=1&ie=UTF-8&sa=X&ved=0ahUKEwjC4cL78v_VAhUjS2MKHXbTBDsQ_AUICigB" target="_blank">100 Galleria Pkwy SE,<br/>
+								Suite 1100<br/>
+								Atlanta, GA 30339</a><!-- internal_address -->
+							
+							<a class="internal_get_directions" href="https://www.google.com/maps?q=100+Galleria+Pkwy+SE,+Suite+1100&oe=utf-8&um=1&ie=UTF-8&sa=X&ved=0ahUKEwjC4cL78v_VAhUjS2MKHXbTBDsQ_AUICigB" target="_blank">Get Directions</a><!-- internal_get_directions -->
+							
+							
+							<div class="map">
+								
+								
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.1831097669083!2d-84.46471118478937!3d33.884937680651724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f51048855a731f%3A0x7573edc841e89147!2s100+Galleria+Pkwy+SE+%231100%2C+Atlanta%2C+GA+30339!5e0!3m2!1sen!2sus!4v1504128475101" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								
+							</div><!-- map -->
+							
+							
+						</div><!-- location_wrapper -->
+						
+					</div><!-- contact_info_right -->
+					
+				</div><!-- contact_info -->
+				
+			</div><!-- contact_wrapper -->
+			
+			
 			
 			
 		</div><!-- internal_content -->
