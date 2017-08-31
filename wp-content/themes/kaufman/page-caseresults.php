f11af2320ab5241ee2333a67a2e2a630324943c4
Garrett Cullen <Garth619@users.noreply.github.com>
2017-08-31 09:59:04 -0700

    push

--- wp-content/themes/kaufman/page-caseresults.php
+++ wp-content/themes/kaufman/page-caseresults.php
@@ -15,12 +15,135 @@
 	
 	<div class="inner_container">
 		
-		<div class="internal_content content">
+		<div class="internal_content">
 			
 			
-			Coming Shortly
-			
-			<?php //get_template_part( 'loop', 'index' );?>
+			<div class="internal_case_results_wrapper">
+				
+				
+				<div class="case_col">
+					
+					<div class="single_case">
+						
+						<span class="case_amount">$25,000,000</span>
+						
+						<span class="case_header">Failure To Timely Diagnose And Treat A Stroke</span><!-- case_header -->
+						
+						<svg id="caseresults_gold_line" viewBox="0 0 136.39 11.05">
+				
+							
+							<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+						</svg><!-- red_line_intro -->
+						
+						<span class="case_description">R. J. was a supervisor at Lockheed Martin, where he managed a large crew of people who respected him immensely...</span><!-- case_description -->
+						
+						<a class="case_read_more" href="">Read More</a><!-- case_read_more -->
+						
+					</div><!-- single_case -->
+					
+					<div class="single_case">
+						
+						<span class="case_amount">$25,000,000</span>
+						
+						<span class="case_header">Failure To Timely Diagnose And Treat A Stroke</span><!-- case_header -->
+						
+						<svg id="caseresults_gold_line" viewBox="0 0 136.39 11.05">
+				
+							
+							<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+						</svg><!-- red_line_intro -->
+						
+						<span class="case_description">R. J. was a supervisor at Lockheed Martin, where he managed a large crew of people who respected him immensely...</span><!-- case_description -->
+						
+						<a class="case_read_more" href="">Read More</a><!-- case_read_more -->
+						
+					</div><!-- single_case -->
+					
+					<div class="single_case">
+						
+						<span class="case_amount">$25,000,000</span>
+						
+						<span class="case_header">Failure To Timely Diagnose And Treat A Stroke</span><!-- case_header -->
+						
+						<svg id="caseresults_gold_line" viewBox="0 0 136.39 11.05">
+				
+							
+							<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+						</svg><!-- red_line_intro -->
+						
+						<span class="case_description">R. J. was a supervisor at Lockheed Martin, where he managed a large crew of people who respected him immensely...</span><!-- case_description -->
+						
+						<a class="case_read_more" href="">Read More</a><!-- case_read_more -->
+						
+					</div><!-- single_case -->
+					
+				</div><!-- case_col -->
+				
+				<div class="case_col">
+					
+					<div class="single_case">
+						
+						<span class="case_amount">$25,000,000</span>
+						
+						<span class="case_header">Failure To Timely Diagnose And Treat A Stroke</span><!-- case_header -->
+						
+						<svg id="caseresults_gold_line" viewBox="0 0 136.39 11.05">
+				
+							
+							<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+						</svg><!-- red_line_intro -->
+						
+						<span class="case_description">R. J. was a supervisor at Lockheed Martin, where he managed a large crew of people who respected him immensely...</span><!-- case_description -->
+						
+						<a class="case_read_more" href="">Read More</a><!-- case_read_more -->
+						
+					</div><!-- single_case -->
+					
+					<div class="single_case">
+						
+						<span class="case_amount">$25,000,000</span>
+						
+						<span class="case_header">Failure To Timely Diagnose And Treat A Stroke</span><!-- case_header -->
+						
+						<svg id="caseresults_gold_line" viewBox="0 0 136.39 11.05">
+				
+							
+							<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+						</svg><!-- red_line_intro -->
+						
+						<span class="case_description">R. J. was a supervisor at Lockheed Martin, where he managed a large crew of people who respected him immensely...</span><!-- case_description -->
+						
+						<a class="case_read_more" href="">Read More</a><!-- case_read_more -->
+						
+					</div><!-- single_case -->
+					
+					<div class="single_case">
+						
+						<span class="case_amount">$25,000,000</span>
+						
+						<span class="case_header">Failure To Timely Diagnose And Treat A Stroke</span><!-- case_header -->
+						
+						<svg id="caseresults_gold_line" viewBox="0 0 136.39 11.05">
+				
+							
+							<polyline class="red_line_intro_path" points="135.79 10.24 123.59 1.25 112.49 9.45 101.39 1.25 90.29 9.45 79.29 1.25 68.19 9.45 57.09 1.25 45.99 9.45 34.89 1.25 23.89 9.45 12.79 1.25 0.59 10.24"/>
+					
+						</svg><!-- red_line_intro -->
+						
+						<span class="case_description">R. J. was a supervisor at Lockheed Martin, where he managed a large crew of people who respected him immensely...</span><!-- case_description -->
+						
+						<a class="case_read_more" href="">Read More</a><!-- case_read_more -->
+						
+					</div><!-- single_case -->
+					
+				</div><!-- case_col -->
+				
+			</div><!-- internal_case_results_wrapper -->
 			
 			
 		</div><!-- internal_content -->
